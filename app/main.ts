1c9011d254644af4d5c180646c8a78957ae87343
Max Besley <61967895+maxbesley@users.noreply.github.com>
2025-06-14 14:50:27 +1000

    Change port again

--- app/main.ts
+++ app/main.ts
@@ -10,4 +10,4 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-await start(manifest, { server: { port: 3000 } });
+await start(manifest, { server: { port: 9000 } });
