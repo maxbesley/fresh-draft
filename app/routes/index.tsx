08c0aac11d2a5b383698ccab6c010c765cd46b83
Max Besley <61967895+maxbesley@users.noreply.github.com>
2025-06-17 15:36:55 +1000

    Try to change background colour again

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#c41475]">
+    <div class="px-4 py-8 mx-auto bg-violet-400">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
@@ -15,7 +15,6 @@ export default function Home() {
         />
         <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
         <p class="my-4">
-          Hi there!!!
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
         </p>
