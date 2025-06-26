b6dbe4515a91edbcc403e2072d479f1e7b22edbf
Max Besley <61967895+maxbesley@users.noreply.github.com>
2025-06-26 12:11:41 +1000

    Update Deno server slightly

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 const body = `
-  <html>
-  <pre>
+<html>
+<pre>
   Once a guy stood all day shaking bugs from his hair.
   The doctor told him there were no bugs in his hair.
   After he had taken a shower for eight hours, standing
@@ -8,11 +8,12 @@ const body = `
   the bugs, he got out and dried himself, and he still
   had bugs in his hair; in fact, he had bugs all over him.
   A month later he had bugs in his lungs.
-  </pre>
-  </html>
+</pre>
+</html>
 `;
 
 const headers = {
+  "X-Powered-By": "deno",
   "content-type": "text/html",
 };
 
